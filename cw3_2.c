6fd972f0beff3c032dd84a81b0c5a3b7d1179434
Daria Kręcichwost <daria.krecichwost@student.up.krakow.pl>
2021-04-24 14:37:50 +0000

    poprawki formatowania

--- cw3_2.c
+++ cw3_2.c
@@ -4,11 +4,13 @@
 int main()
 {
     char slowo_1[ 50 ],slowo_2[50];
-        printf("Podaj tekst: ");
-        scanf("%s",&slowo_1);
-        printf("%s\n",&slowo_1);
-        printf("Podaj slowo: ");
-        scanf("%s",&slowo_2);
+    
+    printf("Podaj tekst: ");
+    scanf("%s",&slowo_1);
+    printf("%s\n",&slowo_1);
+    printf("Podaj slowo: ");
+    scanf("%s",&slowo_2);
+    
     printf( "Polaczone slowo to: %s", strcat(slowo_1, slowo_2));
     return 0;
 }
