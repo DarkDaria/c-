63f529b2328485b45f8cb619e536ccdad5692308
Daria Kręcichwost <daria.krecichwost@student.up.krakow.pl>
2021-05-06 20:05:08 +0000

    menu z wyświetlaniem tablicy (kolumn,wierszy i połówek tablicy)

--- cw1.c
+++ cw1.c
@@ -1,12 +1,10 @@
-//Musze wykonac refaktor kodu bo duzo funkcji jest do siebie podobnych a moze da sie optymalizowac.
-
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
 
 #define N 6
 
-void menu();
+int menu();
 
 void wypisz(double [][N], int n);
 
@@ -16,54 +14,63 @@ int wskaz_wiersz();
 
 int wskaz_kolumne();
 
-int wskaz_przekatna();
-
 void wypisz_wiersz(double [][N], int n);
 
 void wypisz_kolumne(double [][N], int n);
 
-void wyswietl_przekatna(double tab2[][N], int n);
+void wyswietl_przekatna_lewa_gora(double [][N], int n);
+
+void wyswietl_przekatna_prawy_dol(double [][N], int n);
 
 int main() {
-    double t2w[N][N];
     menu();
     return 0;
 }
 
-void menu() {
-    int numer_zadania, numer_kolumny, numer_wiersza, rodzaj_przekatnej;
+int menu() {
+    int numer_zadania, numer_kolumny, numer_wiersza;
     double t2w[N][N];
-    printf("Witaj w menu,\nWybierz numer programu:");
-    printf("\n 1.Wyswietl wiersz\n 2.Wyswietl kolumne\n 3.Wyswietl przekatna\nPodaj numer: ");
-    scanf("%d", &numer_zadania);
-    printf("\nWybrany numer zadania: %d\n", numer_zadania);
-    switch (numer_zadania) {
-        case 1: {
-            wypelnij_losowymi(t2w, N);
-            wypisz(t2w, N);
-            numer_wiersza = wskaz_wiersz();
-            wypisz_wiersz(t2w, numer_wiersza);
+    do {
+        printf("\n\n\tWitaj w menu!\nWybierz numer programu:");
+        printf("\n\t1.Wyswietl wiersz.\n\t2.Wyswietl kolumne.\n\t3.Wyswietl lewa polowe tablicy.\n\t4.Wyswietl prawa polowe tabilcy.\n\nWpisz 0 aby zakonczyc program\nPodaj numer: ");
+        scanf("%d", &numer_zadania);
+        printf("\nWybrany numer zadania: %d\n", numer_zadania);
+        switch (numer_zadania) {
+            case 1: {
+                wypelnij_losowymi(t2w, N);
+                wypisz(t2w, N);
+                numer_wiersza = wskaz_wiersz();
+                wypisz_wiersz(t2w, numer_wiersza);
+                break;
+            }
+            case 2:
+                wypelnij_losowymi(t2w, N);
+                wypisz(t2w, N);
+                numer_kolumny = wskaz_kolumne();
+                wypisz_kolumne(t2w, numer_kolumny);
+                break;
+            case 3:
+                wypelnij_losowymi(t2w, N);
+                wypisz(t2w, N);
+                printf("\n");
+                wyswietl_przekatna_lewa_gora(t2w, N);
+                break;
+            case 4:
+                wypelnij_losowymi(t2w, N);
+                wypisz(t2w, N);
+                wyswietl_przekatna_prawy_dol(t2w, N);
+                break;
+            case 0:
+                printf("Koniec programu");
+                break;
+            default:
+                printf("Nie ma takiej opcji! Prosze wybrac inna\n");
         }
-            break;
-        case 2:
-            wypelnij_losowymi(t2w, N);
-            wypisz(t2w, N);
-            numer_kolumny = wskaz_kolumne();
-            wypisz_kolumne(t2w, numer_kolumny);
-            break;
-        case 3:
-        //przekatna wyswietlanie plus zapytanie od ktorej strony DO DOKOŃCZENIA
-            wypelnij_losowymi(t2w, N);
-            wypisz(t2w, N);
-            rodzaj_przekatnej = wskaz_przekatna();
-            wyswietl_przekatna(t2w, rodzaj_przekatnej);
-            break;
-        default:
-            printf("Nie ma takiej opcji! Prosze wybrac inna\n");
-    }
+    } while (numer_zadania != 0);
 }
 
 void wypisz(double tab2[][N], int n) {
+    printf("Twoja wygenerowana tablica:\n");
     int i, j;
     for (i = 0; i < n; i++) {
         for (j = 0; j < n; j++) {
@@ -73,7 +80,7 @@ void wypisz(double tab2[][N], int n) {
     }
 }
 
-void wypelnij_losowymi(double tab2[][6], int n) {
+void wypelnij_losowymi(double tab2[][N], int n) {
     srand(time(0));
 
     for (int i = 0; i < n; i++) {
@@ -97,33 +104,42 @@ int wskaz_kolumne() {
     return --numer;
 }
 
-int wskaz_przekatna() {
-    int numer;
-    printf("Przekatna po lewej numer 1,Przekadna od prawej numer 2: \n");
-    scanf("%d", &numer);
-    return numer;
-}
-
 void wypisz_wiersz(double tab2[][N], int n) {
-
+    printf("Twoj wybrany wiersz\n");
     for (int i = 0; i < N; i++) {
         printf("%8.2f", tab2[n][i]);
     }
 }
 
 void wypisz_kolumne(double tab2[][N], int n) {
-
+    printf("Twoja wybrana kolumna\n");
     for (int i = 0; i < N; i++) {
         printf("%8.2f\n", tab2[i][n]);
     }
 }
 
-void wyswietl_przekatna(double tab2[][N], int n) {
-    for (int i = n; i <= N; ++i) {
-        for (int j = i; j <= N; ++j) {
-            printf("%2.f ", j);
+void wyswietl_przekatna_lewa_gora(double tab2[][N], int n) {
+    printf("Twoja polowa tablicy\n");
+    int j, i;
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n - i; j++) {
+            printf("%8.2f", tab2[i][j]);
         }
-
         printf("\n");
     }
+}
+
+void wyswietl_przekatna_prawy_dol(double tab2[][N], int n) {
+    printf("Twoja polowa tablicy\n");
+    int j, i;
+    for (i = 0; i < n; i++) {
+        for (int k = 0; k < i; k++) {
+            printf("\t");
+        }
+        for (j = i; j < n; j++) {
+            printf("%8.2f", tab2[i][j]);
+        }
+        printf("\n");
+    }
+
 }
